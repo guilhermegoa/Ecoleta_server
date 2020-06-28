492f3ea11ecdf47e5f1668e8f43e87c2b4a648fd
Guilherme Oliveira <guilhergoa@hotmail.com>
2020-06-28 16:53:42 -0300

    fifth day

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,26 @@
 import express from "express";
 
+import multer from "multer";
+import multerConfig from "./config/multer";
+
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 
-//index(listagem), show/store, create, update, delete/destroy
-
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get("/items", itemsController.index);
-
-routes.post("/points", pointsController.create);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
+routes.post("/points", upload.single("image"), pointsController.create);
+
 export default routes;
+
+//index(listagem), show/store, create, update, delete/destroy
 
 //Service Pattern
 //RepositoryPattern (Data Mapper)
